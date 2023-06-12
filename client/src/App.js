1a075cee7f2b2bd1c05795b0060082de7ff13fdf
laksman <dielaksman98@gmail.com>
2023-06-12 21:17:09 +0700

    client

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import Home from "./components/Home";
+import Create from "./components/Create";
+import Edit from "./components/Edit";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Form Users</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/update" element={<Edit />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Player.jsx
@@ -0,0 +1,32 @@
+const Player = [
+  {
+    id: "1",
+    username: "dodon",
+    email: "dodon@gmail.com",
+    password: "123",
+    experience: "2000",
+  },
+  {
+    id: "2",
+    username: "didin",
+    email: "didin@gmail.com",
+    password: "abcd",
+    experience: "5000",
+  },
+  {
+    id: "3",
+    username: "dudun",
+    email: "dudun@gmail.com",
+    password: "test222",
+    experience: "1000",
+  },
+  {
+    id: "4",
+    username: "deden",
+    email: "deden@gmail.com",
+    password: "23234",
+    experience: "1000",
+  },
+];
+
+export default Player;
